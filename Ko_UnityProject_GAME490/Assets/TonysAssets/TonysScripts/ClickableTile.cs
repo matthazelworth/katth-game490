0b280ce041c35afd185d3120eafc4ffb5bea8f93
AnthonyBClavel <abclavel123@gmail.com>
2020-09-26 18:05:35 -0400

    I give up

---
+++ Ko_UnityProject_GAME490/Assets/TonysAssets/TonysScripts/ClickableTile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableTile : MonoBehaviour
+{
+    public int tileX;
+    public int tileY;
+    public TileMap map;
+
+    void OnMouseDown()
+    {
+        Debug.Log("Click!");
+
+        map.MoveSelectedUnitTo(tileX, tileY);
+    }
+}
---
+++ Ko_UnityProject_GAME490/Assets/TonysAssets/TonysScripts/Unit.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public int tileX;
+    public int tileY;
+}
